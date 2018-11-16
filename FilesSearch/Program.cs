6c0684b1de3d90ad2b43ea77cdbce808a1b78014
Alexander Pak <pak@mercury.office>
2018-11-16 11:48:53 +0400

    init

---
+++ FilesSearch/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilesSearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine("Введите тип файла:");
+            var pattern = "*" + Console.ReadLine();
+            Console.WriteLine("Введите путь:");
+            string path;
+            try
+            {
+                while ((path = Console.ReadLine()) != "")
+                {
+                    Handle(path, pattern);
+                }
+            }
+            catch (DirectoryNotFoundException e)
+            {
+                Console.WriteLine("Error! {0}", e.Message);
+            }
+        }
+
+        static void Handle(string path, string pattern)
+        {
+            if (!Directory.Exists(path))
+                throw new DirectoryNotFoundException("Путь неверен");
+
+            var files = Directory.GetFiles(path, pattern, SearchOption.AllDirectories);
+            foreach (var file in files)
+            {
+                Console.WriteLine(file);
+            }
+        }
+    }
+}
