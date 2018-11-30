4f788d5d879ad61d9efe81d67929168a752e2178
Alexander Pak <pak@mercury.office>
2018-11-30 10:54:39 +0400

    Added a signature for docx and pptx

--- FilesSearch/Program.cs
+++ FilesSearch/Program.cs
@@ -11,33 +11,54 @@ namespace FilesSearch
     {
         static void Main(string[] args)
         {
-            var files = Directory.GetFiles("../../testFolder", "image*", SearchOption.AllDirectories);
-            var signature = getSignature("../../testFolder/image.jpg");
-            var result = files.Where(file => getSignature(file) == signature);
+            var files = Directory.GetFiles("../../testFolder", "*", SearchOption.AllDirectories);
 
-            foreach (var item in result)
+            foreach (var item in files)
             {
-                Console.WriteLine(item);
+                Console.Write(item + " -- ");
+                getSignature(item);
             }
         }
 
-        static string getSignature(string myFile)
+        static void getSignature(string myFile)
         {
             string signature = null;
             using (var fs = new FileStream(myFile, FileMode.Open))
             {
-                var buffer = new byte[100];
+                var buffer = new byte[10];
                 fs.Read(buffer, 0, buffer.Length);
                 signature = string.Join(" ", buffer.Select(b => b.ToString("X2")));
-                //Console.WriteLine(signature);
-
-                /*if (signature.StartsWith("FF D8 FF E0"))
-                    Console.WriteLine("JPEG");
-
-                if (signature.StartsWith("89 50 4E 47 0D 0A 1A 0A"))
-                    Console.WriteLine("PNG");*/
+                // Console.WriteLine(signature);
+                switch (signature)
+                {
+                    case "FF D8 FF E0 00 10 4A 46 49 46":
+                        Console.WriteLine("JPEG");
+                        break;
+                    case "89 50 4E 47 0D 0A 1A 0A":
+                        Console.WriteLine("PNG");
+                        break;
+                    case "50 4B 03 04 14 00 06 00 08 00":
+                        buffer = new byte[6];
+                        fs.Read(buffer, 0, buffer.Length);
+                        signature = string.Join(" ", buffer.Select(b => b.ToString("X2")));
+                        switch (signature)
+                        {
+                            case "00 00 21 00 DF A4":
+                                Console.WriteLine("DOCX");
+                                break;
+                            case "00 00 21 00 DF CC":
+                                Console.WriteLine("PPTX");
+                                break;
+                            default:
+                                Console.WriteLine("Unknown");
+                                break;
+                        }
+                        break;
+                    default:
+                        Console.WriteLine("Unknown");
+                        break;
+                }
             }
-            return signature;
         }
     }
 }
